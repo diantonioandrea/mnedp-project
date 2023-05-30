fbd2c6c35a52e3de586563f3df8563e8d3d8e6ed
Andrea Di Antonio <mail@diantonioandrea.com>
2023-05-30 15:32:46 +0200

    Started working on main program

---
+++ main/main.m
@@ -0,0 +1,82 @@
+% I prefere using a function not to have variables in the
+% workspace.
+function main
+	addpath("../src")
+	
+	%% Initialization
+	% Functions.
+	u_a = @(a, x) x.^a - x.^(a + 1); % Exact solution u. a: alpha.
+	up_a = @(a, x) a * x.^(a - 1) - (a + 1) * x.^a; % u';
+	f_a = @(a, x) - a * (a - 1) * x.^(a - 2) + ...
+		(a + 1) * a * x.^(a - 1); % -u'' from Poisson.
+
+	% Starting mesh.
+	sMesh = builder(5); % Built with 4 starting elements.
+
+	% Errors.
+	errors = zeros(2, 10);
+
+	%% Alpha = 5/3.
+	u = @(x) u_a(5/3, x);
+	up = @(x) up_a(5/3, x);
+	f = @(x) f_a(5/3, x);
+
+	firstMesh = sMesh;
+
+	fprintf("Valutazione degli errori con alpha = 5/3.\n")
+	
+	j = 1;
+	[uh, ~, ~] = solver(firstMesh, f);
+	errors(1, j) = errorEstimate(firstMesh, up, uh);
+
+	fprintf("\nElements: %d\tError: %e.", ...
+			length(firstMesh.elements), errors(1, j));
+
+	for j = 2:10
+		firstMesh = refiner(firstMesh, []);
+		[uh, ~, ~] = solver(firstMesh, f);
+		errors(1, j) = errorEstimate(firstMesh, up, uh);
+		
+		fprintf("\nElements: %d\tError: %e.", ...
+			length(firstMesh.elements), errors(1, j));
+	end
+
+	%% Alpha = 10.
+	u = @(x) u_a(10, x);
+	up = @(x) up_a(10, x);
+	f = @(x) f_a(10, x);
+
+	secondMesh = sMesh;
+
+	fprintf("\n\nValutazione degli errori con alpha = 10.\n")
+
+	j = 1;
+	[uh, ~, ~] = solver(secondMesh, f);
+	errors(2, j) = errorEstimate(secondMesh, up, uh);
+
+	fprintf("\nElements: %d\tError: %e.", ...
+			length(secondMesh.elements), errors(2, j));
+
+	for j = 2:10
+		marked = marker(secondMesh, f);
+		secondMesh = refiner(secondMesh, marked);
+		[uh, ~, ~] = solver(secondMesh, f);
+		errors(2, j) = errorEstimate(secondMesh, up, uh);
+		
+		fprintf("\nElements: %d\tError: %e.", ...
+			length(secondMesh.elements), errors(2, j));
+	end
+end
+
+function err = errorEstimate(mesh, up, uh)
+	err = 0;
+
+	for j = 1:length(mesh.nodes) - 1
+		h = mesh.elements(j, 3);
+		xs = mesh.nodes(j);
+		xd = mesh.nodes(j + 1);
+
+		err = err + .5 * h * sqrt((up(xs) - uh(j))^2 + ...
+			(up(xd) - uh(j + 1))^2);
+	end
+end
