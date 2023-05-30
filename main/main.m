977416d42772d7ba6822776a9e714cb47c002743
Andrea Di Antonio <mail@diantonioandrea.com>
2023-05-30 16:51:55 +0200

    Update main.m

--- main/main.m
+++ main/main.m
@@ -1,5 +1,4 @@
-% I prefere using a function not to have variables in the
-% workspace.
+% Andrea Di Antonio.
 function main
 	addpath("../src")
 	
@@ -9,24 +8,25 @@ function main
 	f_a = @(a, x) - a * (a - 1) * x.^(a - 2) + ...
 		(a + 1) * a * x.^(a - 1); % -u'' from Poisson.
 
-	% Starting mesh.
-	sMesh = builder(5); % Built with 4 starting elements.
-
 	% Tests.
 	steps = 10;
-	errors = zeros(2, steps);
+	sizes = zeros(4, steps);
+	errors = zeros(4, steps);
 
 	%% Alpha = 5/3.
 	up = @(x) up_a(5/3, x);
 	f = @(x) f_a(5/3, x);
-
-	firstMesh = sMesh;
+	
+	% Test 1.
+	firstMesh = builder(5);
 
 	fprintf("1. Valutazione degli errori con alpha = 5/3.\nMetodo semplice.\n")
 	
 	j = 1;
 	[uh, ~, ~] = solver(firstMesh, f);
+
 	errors(1, j) = errorEstimate(firstMesh, up, uh);
+	sizes(1, j) = length(firstMesh.elements);
 
 	fprintf("\nElements: %d\tError: %e.", ...
 			length(firstMesh.elements), errors(1, j));
@@ -34,19 +34,24 @@ function main
 	for j = 2:steps
 		firstMesh = refiner(firstMesh, []);
 		[uh, ~, ~] = solver(firstMesh, f);
+
 		errors(1, j) = errorEstimate(firstMesh, up, uh);
+		sizes(1, j) = length(firstMesh.elements);
 		
 		fprintf("\nElements: %d\tError: %e.", ...
 			length(firstMesh.elements), errors(1, j));
 	end
 	
-	secondMesh = sMesh;
+	% Test 2.
+	secondMesh = builder(5);
 
 	fprintf("\n\nMetodo adattivo.\n")
 
 	j = 1;
 	[uh, ~, ~] = solver(secondMesh, f);
+
 	errors(2, j) = errorEstimate(secondMesh, up, uh);
+	sizes(2, j) = length(secondMesh.elements);
 
 	fprintf("\nElements: %d\tError: %e.", ...
 			length(secondMesh.elements), errors(2, j));
@@ -55,7 +60,9 @@ function main
 		marked = marker(secondMesh, f);
 		secondMesh = refiner(secondMesh, marked);
 		[uh, ~, ~] = solver(secondMesh, f);
+
 		errors(2, j) = errorEstimate(secondMesh, up, uh);
+		sizes(2, j) = length(secondMesh.elements);
 		
 		fprintf("\nElements: %d\tError: %e.", ...
 			length(secondMesh.elements), errors(2, j));
@@ -63,46 +70,70 @@ function main
 	%% Alpha = 10.
 	up = @(x) up_a(10, x);
 	f = @(x) f_a(10, x);
-
-	firstMesh = sMesh;
+	
+	% Test 3.
+	firstMesh = builder(5);
 
 	fprintf("\n\n2.Valutazione degli errori con alpha = 10.\nMetodo semplice.\n")
 	
 	j = 1;
 	[uh, ~, ~] = solver(firstMesh, f);
-	errors(1, j) = errorEstimate(firstMesh, up, uh);
+	
+	errors(3, j) = errorEstimate(firstMesh, up, uh);
+	sizes(3, j) = length(firstMesh.elements);
 
 	fprintf("\nElements: %d\tError: %e.", ...
-			length(firstMesh.elements), errors(1, j));
+			length(firstMesh.elements), errors(3, j));
 
 	for j = 2:steps
 		firstMesh = refiner(firstMesh, []);
 		[uh, ~, ~] = solver(firstMesh, f);
-		errors(1, j) = errorEstimate(firstMesh, up, uh);
+		
+		errors(3, j) = errorEstimate(firstMesh, up, uh);
+		sizes(3, j) = length(firstMesh.elements);
 		
 		fprintf("\nElements: %d\tError: %e.", ...
-			length(firstMesh.elements), errors(1, j));
+			length(firstMesh.elements), errors(3, j));
 	end
 
-	secondMesh = sMesh;
+	% Test 4.
+	secondMesh = builder(5);
 
 	fprintf("\n\nMetodo adattivo.\n")
 
 	j = 1;
 	[uh, ~, ~] = solver(secondMesh, f);
-	errors(2, j) = errorEstimate(secondMesh, up, uh);
+	
+	errors(4, j) = errorEstimate(secondMesh, up, uh);
+	sizes(4, j) = length(secondMesh.elements);
 
 	fprintf("\nElements: %d\tError: %e.", ...
-			length(secondMesh.elements), errors(2, j));
+			length(secondMesh.elements), errors(4, j));
 
 	for j = 2:steps
 		marked = marker(secondMesh, f);
 		secondMesh = refiner(secondMesh, marked);
 		[uh, ~, ~] = solver(secondMesh, f);
-		errors(2, j) = errorEstimate(secondMesh, up, uh);
+		
+		errors(4, j) = errorEstimate(secondMesh, up, uh);
+		sizes(4, j) = length(secondMesh.elements);
 		
 		fprintf("\nElements: %d\tError: %e.", ...
-			length(secondMesh.elements), errors(2, j));
+			length(secondMesh.elements), errors(4, j));
+	end
+
+	%% Graphics.
+
+	tiledlayout(2, 1);
+	for j = 1:2:4
+		nexttile;
+		loglog(sizes(j, :), errors(j, :), DisplayName="Simple");
+		hold on;
+
+		loglog(sizes(j + 1, :), errors(j + 1, :), DisplayName="Adaptive.");
+
+		hold off;
+		legend;
 	end
 end
 
@@ -114,7 +145,9 @@ function err = errorEstimate(mesh, up, uh)
 		xs = mesh.nodes(j);
 		xd = mesh.nodes(j + 1);
 
-		err = err + .5 * h * sqrt((up(xs) - uh(j))^2 + ...
-			(up(xd) - uh(j + 1))^2);
+		gh = (uh(j + 1) - uh(j)) / h;
+
+		err = err + .5 * h * sqrt((up(xs) - gh)^2 + ...
+			(up(xd) - gh)^2);
 	end
 end
